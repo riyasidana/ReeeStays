6f886efafcb68bfa839408f39e38ea186a3fc80d
Riya Sidana <riyasidana1504@gmail.com>
2026-02-21 16:44:48 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {Route, Routes} from "react-router-dom";
+import IndexPage from './pages/IndexPage';
+
+function App() {
+  return (
+    <Routes>
+     <Route index element={<IndexPage />} />
+    </Routes>
+  )
+}
+
+export default App
